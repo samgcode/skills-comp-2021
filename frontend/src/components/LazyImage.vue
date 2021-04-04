c3bfdb0d66cb77f77ffb022be75218c87c6fcbf4
Sam g <samgcode@gmail.com>
2021-04-04 15:33:44 -0600

    - added lazy loading images

---
+++ frontend/src/components/LazyImage.vue
@@ -0,0 +1,34 @@
+<template>
+    <body>
+        <v-lazy-image :src="require(`@/assets/${name}.jpg`)" :src-placeholder="require(`@/assets/lowres/${name}.jpg`)" :alt="name" :class="classes"/>
+    </body>
+</template>
+
+<script>
+import VLazyImage from "v-lazy-image"
+export default {
+    name: 'LazyImage',
+    props: {
+        name: String,
+        classes: String
+    },
+    components: {
+        VLazyImage
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+.v-lazy-image {
+    filter: blur(10px);
+    transition: filter 0.7s;
+}
+.v-lazy-image-loaded {
+    filter: blur(0);
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { VLazyImagePlugin } from "v-lazy-image";
+
+Vue.use(VLazyImagePlugin);
 
 Vue.config.productionTip = false
 
