97c0895a02835ba9fdc628c9b8b5a87bd9d81229
Sam g <samgcode@gmail.com>
2021-04-08 21:22:44 -0600

    - added a changeable map to the home page

--- frontend/src/components/Home/Home.vue
+++ frontend/src/components/Home/Home.vue
@@ -31,77 +31,34 @@
                 <p class="mx-auto max-w-6xl">The Rocky Mountains of Canada are a protected UNESCO World Heritage Site. Here is where Rapid Air captures and bottles some the highest quality air in the world. We want our customers to experience breathing the way it was intended. Free of city pollution, fragrances and waste; our Banff and Lake Louise lines of clean air are unmatchable in quality.</p>
             </div>
             <div class="md:grid-cols-2 mt-12 container grid grid-cols-1 max-w-6xl xl:mx-52 lg:mx-40 md:mx-32 sm:mx-10 mx-3 gap-4">
-                <div class="text-left max-w-md" data-aos="fade-right">
-                    <h2 class="text-blue text-xl">Banf Air</h2>
-                    <p class="py-3">
+                <div class="text-left max-w-md">
+                    <h2 class="text-blue text-xl" data-aos="fade-right">Banf Air</h2>
+                    <p class="py-3" data-aos="fade-right">
                         What’s top of mind when you think about glacier mountains and sprawling green forest? For most, it’s a striking view, an untouched landscape, and deep breaths of pure, natural air. That’s what we pictured when we started thinking about serving the best air to people around the world.
                     </p>
-                    <p class="pt-3">
+                    <p class="pt-3" data-aos="fade-right">
                         We chose to bottle air from Banff because it's so memorable. People who visit this place instantly are drawn into the purity of the nature surrounding them. You breathe deeper and begin feeling healthier. Banff is more than just incredible mountain air, it's an emotional experience that we wanted to share with everyone.
                     </p>
                 </div>
 
-                <div class="text-left max-w-md" data-aos="fade-left">
-                    <h2 class="text-blue text-xl">Lake Louise Air</h2>
-                    <p class="py-3">
+                <div class="text-left max-w-md">
+                    <h2 class="text-blue text-xl" data-aos="fade-left">Lake Louise Air</h2>
+                    <p class="py-3" data-aos="fade-left">
                         For nature lovers, Lake Louise is a favourite destination. A picture perfect emerald lake is crowned by white-tipped mountain ranges. The giant blue lake instantly pulls you in as it contrasts with lush green trees and white tipped Rocky Mountains. In the winter, it becomes a paradise of powdery white snow.
                     </p>
-                    <p class="pt-3">
+                    <p class="pt-3" data-aos="fade-left">
                         Lake Louise was a clear choice for us to bottle our Rocky Mountain air from. It's clean and fresh, but it also makes you feel on top of the world when you're surrounded by a crown of mountain tops. We want our customers to breathe in a sense of wonder along with a feeling of healthy air.
                     </p>
                 </div>
             </div>
         </div>
-        <div class="flex justify-center bg-blue-200 mt-10">
-            <div class="md:grid-cols-2 mt-12 container grid grid-cols-1 max-w-6xl xl:mx-52 lg:mx-40 md:mx-32 sm:mx-10 mx-3 gap-4">
-                <div class="text-left max-w-md" data-aos="fade-up">
-                    <div class="flex">
-                        <img class="block h-11 w-auto pr-2" src="@/assets/temp_map.png" alt="RapidAir">
-                        <h2 class="text-blue text-xl">Check out our Edmonton location</h2>
-                    </div>
-                    <p class="py-3">
-                        Rapid Air has locations with full-service stores in Edmonton
-                    </p>
-                    <a class="underline">
-                        Map
-                    </a>
+        <div class="flex bg-blue-200 mt-10 py-5">
+            <div class="flex flex-wrap xl:mx-52 lg:mx-40 md:mx-32 sm:mx-10 mx-3">
+                <div class="xl:max-w-xl max-w-md xl:mr-3 mr-6" data-aos="fade-up">
+                    <h1 class="text-3xl font-semibold">Check out one of our locations</h1>
+                    <h2 class="text-xl pb-5 pt-2">Rapid Air has 4 locations with full-service stores in Calgary, Edmonton, Lethbridge and Fort McMurray.</h2>
                 </div>
-                <div class="text-left max-w-md" data-aos="fade-up">
-                    <div class="flex">
-                        <img class="block h-11 w-auto pr-2" src="@/assets/temp_map.png" alt="RapidAir">
-                        <h2 class="text-blue text-xl">Check out our Calgary location</h2>
-                    </div>
-                    <p class="py-3">
-                        Rapid Air has locations with full-service stores in Calgary
-                    </p>
-                    <a class="underline">
-                        Map
-                    </a>
-                </div>
-                <div class="text-left max-w-md pt-8" data-aos="fade-up">
-                    <div class="flex">
-                        <img class="block h-11 w-auto pr-2" src="@/assets/temp_map.png" alt="RapidAir">
-                        <h2 class="text-blue text-xl">Check out our Lethbridge location</h2>
-                    </div>
-                    <p class="py-3">
-                        Rapid Air has locations with full-service stores in Lethbridge
-                    </p>
-                    <a class="underline">
-                        Map
-                    </a>
-                </div>
-                <div class="text-left max-w-md pb-10 pt-8" data-aos="fade-up">
-                    <div class="flex">
-                        <img class="block h-11 w-auto pr-2" src="@/assets/temp_map.png" alt="RapidAir">
-                        <h2 class="text-blue text-xl">Check out our Fort McMurray location</h2>
-                    </div>
-                    <p class="py-3">
-                        Rapid Air has locations with full-service stores in Fort McMurray
-                    </p>
-                    <a class="underline">
-                        Map
-                    </a>
-                </div>
+                <store-locations class="xl:max-w-xl max-w-md"/>
             </div>
         </div>        
     </body>
@@ -109,11 +66,13 @@
 
 <script>
 import LazyImage from '@/components/LazyImage'
+import StoreLocations from '@/components/Home/StoreLocations'
 
 export default {
     name: 'Home',
     components: {
-        LazyImage
+        LazyImage,
+        StoreLocations
     },
     data() {
         return {
